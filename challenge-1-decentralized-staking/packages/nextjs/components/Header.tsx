48c131ff7523ec91a40b0a2a2397dbc4725852b4
npastrami <npastrana15@outlook.com>
2024-09-28 05:50:17 -0400

    changed market icon

--- challenge-1-decentralized-staking/packages/nextjs/components/Header.tsx
+++ challenge-1-decentralized-staking/packages/nextjs/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, BugAntIcon, CircleStackIcon, InboxStackIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BuildingLibraryIcon, CircleStackIcon, InboxStackIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -32,7 +32,7 @@ export const menuLinks: HeaderMenuLink[] = [
   {
     label: "Marketplace",
     href: "/marketplace",
-    icon: <BugAntIcon className="h-4 w-4" />,
+    icon: <BuildingLibraryIcon className="h-4 w-4" />,
   },
 ];
 
